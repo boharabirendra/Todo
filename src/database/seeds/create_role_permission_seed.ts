7a4c41d08d402ac537498db100f639b5f78ea961
Birendra Bohara <boharabirendra546@gmail.com>
2024-07-16 09:45:57 +0545

    Update role_permission seed

--- src/database/seeds/create_role_permission_seed.ts
+++ src/database/seeds/create_role_permission_seed.ts
@@ -15,43 +15,43 @@ export function seed(knex: Knex): Promise<void> {
       return knex(TABLE_NAME).insert([
         {
           role_id: 1,
-          permission_id: 10
+          permission_id: 1
         },
         {
           role_id: 1,
-          permission_id: 11
+          permission_id: 2
         },
         {
           role_id: 1,
-          permission_id: 12
+          permission_id: 3
         },
         {
           role_id: 1,
-          permission_id: 13
+          permission_id: 4
         },
         {
           role_id: 1,
-          permission_id: 17
+          permission_id: 8
         },
         {
           role_id: 1,
-          permission_id: 18
+          permission_id: 9
         },
         {
           role_id: 2,
-          permission_id: 14
+          permission_id: 5
         },
         {
           role_id: 2,
-          permission_id: 15
+          permission_id: 6
         },
         {
           role_id: 2,
-          permission_id: 16
+          permission_id: 7
         },
         {
           role_id: 2,
-          permission_id: 17
+          permission_id: 8
         },
        
       ]);
